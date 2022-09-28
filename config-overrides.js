2504084979d2b0eac90f61ee45f9bcbf1887c3fa
andrei <andtrusha0223@gmail.com>
2022-09-28 12:40:55 +0300

    testing env

---
+++ config-overrides.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const webpack = require("webpack");
+const { override, addBabelPlugin } = require("customize-cra");
+
+/**
+ * read configuration based on environment,
+ * write .env file and configuration file.
+ */
+const generateConfiguration = async () => {
+  try {
+    const variables = [
+      `NODE_VERSION="16.5.0"`,
+      "GENERATE_SOURCEMAP=false",
+      "REACT_APP_API_URL=https://main-api-dev.herokuapp.com",
+    ];
+    fs.writeFileSync("./.env", variables.join(os.EOL));
+
+    // const env = process.env.REACT_APP_ENV || "development";
+    // const configuration = require(`./config/config.${env}.json`);
+
+    // console.log(configuration);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  process.exit();
+};
+
+generateConfiguration();
+
+module.exports = override((config) => {
+  config.module.rules.push({
+    test: /\.m?js/,
+    resolve: {
+      fullySpecified: false,
+    },
+  });
+  return config;
+});
